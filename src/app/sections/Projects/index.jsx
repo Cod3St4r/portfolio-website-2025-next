86a9b19ddb2b6ab9c1bc817430b9f0737287c588
Cod3St4r <cgmercado@presby.edu>
2025-08-24 18:51:55 -0400

    working on project cards

--- src/app/sections/Projects/index.jsx
+++ src/app/sections/Projects/index.jsx
@@ -1,6 +1,56 @@
+"use client"
+
+import React from "react";
+import "./styles.scss";
+
+const projects =[
+    {
+        imgSrc: '/images/frpic.jpg',
+        title: 'Pokemon Red',
+        description:'Pokémon Red Version is a foundational 1996 Game Boy role-playing video game developed by Game Freak and published by Nintendo. Players travel through the Kanto region, capturing and training a variety of Pokémon, battling Gym Leaders, and preventing Team Rocket from stealing Pokémon to become the greatest trainer.',
+        gitLink:'https://en.wikipedia.org/wiki/Pok%C3%A9mon_Red,_Blue,_and_Yello',
+        projectLink:'https://arcadespot.com/game/pokemon-red/',
+    },
+    {
+        imgSrc: '/images/ygo5ds.jpg',
+        title: '5ds Tagforce',
+        description:"Yu-Gi-Oh! 5D's Tag Force 4 is a card battle strategy game for the PlayStation Portable (PSP) released by Konami in 2009, featuring over 3,500 cards and focusing on the story and characters from the Yu-Gi-Oh! 5D's animated series.",
+        gitLink:"https://yugioh.fandom.com/wiki/Yu-Gi-Oh!_5D%27s_Tag_Force_4",
+        projectLink:"https://www.emulatorgames.net/roms/playstation-portable/yu-gi-oh-5ds-tag-force-4/",
+    },
+    {
+        imgSrc: '/images/titanfall.jpg',
+        title: 'TitanFall',
+        description:"Titanfall is a media franchise that mainly features first-person shooter games. The series was created by Respawn Entertainment and debuted on Xbox and Microsoft Windows; it has expanded to other consoles and platforms.",
+        gitLink:"https://en.wikipedia.org/wiki/Titanfall",
+        projectLink:"https://www.ea.com/en/games/titanfall/titanfall-2",
+    }
+]
+
+
 const Projects = () => {
     return(
-        <h1>Projects</h1>
+        <div className='Page'> 
+            <h1 className='Page__Title'>Projects</h1>
+            <ul className='Page__Projects'>
+                {
+                    projects.map((item,key) =>(
+                        <li key={key}>
+                            <div className='Page__Projects__Card'>
+                                <img className='Page__Projects__Card__Image' src={item.imgSrc}></img>
+                                <h2 className ='Page__Projects__Card__Title'>{item.title}</h2>
+                                <p className='Page__Projects__Card__Description'>{item.description}</p>
+                                <div className='Page__Projects__Card__Buttons'>
+                                    <button className='Page__Projects__Card__Buttons__Github' type="button" onClick={()=> window.open(item.gitLink, '_blank')}>Github</button>
+                                    <button className='Page__Projects__Card__Buttons__Website' type="button" onClick={()=> window.open(item.projectLink, '_blank')}>Website</button>
+                                </div>
+                            </div>
+                        </li>
+                    ))
+                
+}    
+            </ul>
+        </div>
 
     )
 }
